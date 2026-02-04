3e2566853c66c981be9ef00bcea0bdcfd0d4c8cb
abhassomkuwar <abhassomkuwar7@gmail.com>
2026-02-04 21:44:21 +0530

    first commit

---
+++ client/src/pages/Generator.tsx
@@ -0,0 +1,28 @@
+import Title from "../components/Title"
+
+const Generator = () => {
+  return (
+    <div className="min-h-screen text-white p-6 md:p-12 mt-28">
+      <form className="max-w-4xl mx-auto mb-40">
+        <Title heading='Create In-Context Image' description='Upload your model and product images to generate stunning UGC, short-form videos and social media posts'/>
+
+        <div className="flex gap-20 max-sm:flex-col items-start justify-between">
+            <div className="flex flex-col w-full sm:max-w-60 gap-8 mt-8 mb-12">
+                <p>product image</p>
+                <p>product image</p>
+            </div>
+
+
+            <div>
+                <p>Right col</p>
+            </div>
+        </div>
+
+
+
+      </form>
+    </div>
+  )
+}
+
+export default Generator
